006bd21823b699a370c670f09372330ac3cd5440
davidkupyn <davidkupyn@gmail.com>
2023-10-10 16:01:20 +0200

    Add Email Verification

--- src/routes/email-verification/+page.svelte
+++ src/routes/email-verification/+page.svelte
@@ -1,15 +1,67 @@
-<script lang="ts">
+<script>
 	import { enhance } from '$app/forms';
+	import { scale } from 'svelte/transition';
+	import Button from '$lib/components/ui/button.svelte';
+	import { Card } from '$lib/components/ui/card';
+	import { CheckCircle2, Mail } from 'lucide-svelte';
 
-	export let form;
+	let isEmailResent = false;
+
+	$: {
+		if (isEmailResent)
+			setTimeout(() => {
+				isEmailResent = false;
+			}, 5000);
+	}
 </script>
 
-<h1>Email verification</h1>
-<p>Your email verification link was sent to your inbox (i.e. console).</p>
-<h2>Resend verification link</h2>
-<form method="post" use:enhance>
-	<input type="submit" value="Resend" />
-</form>
-{#if form?.success}
-	<p>Your verification link was resent</p>
-{/if}
+<main class="flex min-h-[calc(100dvh-6.5rem)] pt-24 md:pt-32 flex-col items-center px-6">
+	<div class="max-w-md mb-56 w-full" in:scale={{ duration: 400, opacity: 0.9, start: 0.98 }}>
+		<Card let:Header let:Footer class="w-full">
+			<Header let:Title let:Description class="text-center space-y-4">
+				<Mail class="mx-auto text-accent" size="32" />
+				<Title>Email verification</Title>
+				<Description>
+					A verification link has been sent to your email address. Please click on the link that has
+					just been sent to your email account to verify your email and finish the registration.
+				</Description>
+			</Header>
+			<form
+				use:enhance
+				method="POST"
+				class="space-y-5"
+				on:submit={() => {
+					console.log('yo');
+
+					setTimeout(() => {
+						console.log('yo');
+						isEmailResent = false;
+					}, 100);
+				}}
+			>
+				<Footer>
+					{#if isEmailResent}
+						<p
+							class="mx-auto flex text-sm items-center h-9"
+							in:scale={{ start: 0.95, duration: 300 }}
+						>
+							<CheckCircle2 class="mr-2 text-success" size="20" />
+							Your verification link was resent
+						</p>
+					{:else}
+						<div class="w-full" in:scale={{ start: 0.95, duration: 300 }}>
+							<Button
+								type="submit"
+								class="w-full"
+								variant="secondary"
+								on:click={() => (isEmailResent = true)}
+							>
+								Resend
+							</Button>
+						</div>
+					{/if}
+				</Footer>
+			</form>
+		</Card>
+	</div>
+</main>
